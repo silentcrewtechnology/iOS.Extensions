e763d72fd04e2d9f7cb9fbfb48f69c7336246bb0
Васин Валерий Михайлович <vasinvm@akbarsdigital.ru>
2024-08-14 15:35:00 +0300

    перенес расширения из дизайн системы

--- Sources/Extensions/String.swift
+++ Sources/Extensions/String.swift
@@ -12,7 +12,6 @@ public extension String {
         let attributedString = NSMutableAttributedString(string: self)
         return attributedString
     }
-
 }
 
 public extension StringProtocol {
@@ -252,18 +251,5 @@ public extension String {
         } catch {
             return nil
         }
-    }
-}
-
-public extension String {
-    
-    var sha1Base64: String {
-        guard let data = data(using: .utf8) else { return "" }
-        return Data(Insecure.SHA1.hash(data: data)).base64EncodedString()
-    }
-    
-    var sha256Base64: String {
-        guard let data = data(using: .utf8) else { return "" }
-        return Data(SHA256.hash(data: data)).base64EncodedString()
     }
 }
