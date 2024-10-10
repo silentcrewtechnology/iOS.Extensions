932065953237cf6cf68b71ed1b1a5f6e6a83c5cc
KaluginAA <KaluginAA@akbarsdigital.ru>
2024-10-10 15:24:21 +0300

    Добавил SelectorAction

--- Sources/Objects/TapGestureRecognizer.swift
+++ Sources/Objects/TapGestureRecognizer.swift
@@ -8,26 +8,11 @@
 import UIKit
 
 public final class TapGestureRecognizer: UITapGestureRecognizer {
-    
-    final public class Action: NSObject {
-        @objc
-        private func selectorAction() { didTap() }
-        private let didTap: () -> Void
-        
-        public var selector: Selector {
-            #selector(selectorAction)
-        }
-        
-        public init(didTap: @escaping () -> Void) {
-            self.didTap = didTap
-            super.init()
-        }
-    }
-    
-    private let action: Action
+    private let action: SelectorAction
     
     public init(didTap: @escaping () -> Void) {
         self.action = .init(didTap: didTap)
+        
         super.init(
             target: self.action,
             action: self.action.selector
